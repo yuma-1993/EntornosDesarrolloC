67b2d56c97f9d1a9f3b2e563141d5fcef6a9f573
yuma-1993 <110123206+yuma-1993@users.noreply.github.com>
2023-02-05 16:30:52 +0100

    Practica 1 - Fuentes

---
+++ Practica1Fuentes/Practica1Fuentes/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Practica1Fuentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNegrita = new System.Windows.Forms.Button();
+            this.btnCursiva = new System.Windows.Forms.Button();
+            this.btnSubrayado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "En un lugar de la mancha";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnNegrita
+            // 
+            this.btnNegrita.Location = new System.Drawing.Point(57, 96);
+            this.btnNegrita.Name = "btnNegrita";
+            this.btnNegrita.Size = new System.Drawing.Size(75, 23);
+            this.btnNegrita.TabIndex = 1;
+            this.btnNegrita.Text = "Negrita";
+            this.btnNegrita.UseVisualStyleBackColor = true;
+            this.btnNegrita.Click += new System.EventHandler(this.btnNegrita_Click);
+            // 
+            // btnCursiva
+            // 
+            this.btnCursiva.Location = new System.Drawing.Point(159, 96);
+            this.btnCursiva.Name = "btnCursiva";
+            this.btnCursiva.Size = new System.Drawing.Size(75, 23);
+            this.btnCursiva.TabIndex = 2;
+            this.btnCursiva.Text = "Cursiva";
+            this.btnCursiva.UseVisualStyleBackColor = true;
+            this.btnCursiva.Click += new System.EventHandler(this.btnCursiva_Click);
+            // 
+            // btnSubrayado
+            // 
+            this.btnSubrayado.Location = new System.Drawing.Point(269, 96);
+            this.btnSubrayado.Name = "btnSubrayado";
+            this.btnSubrayado.Size = new System.Drawing.Size(75, 23);
+            this.btnSubrayado.TabIndex = 3;
+            this.btnSubrayado.Text = "Subrayado";
+            this.btnSubrayado.UseVisualStyleBackColor = true;
+            this.btnSubrayado.Click += new System.EventHandler(this.btnSubrayado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 165);
+            this.Controls.Add(this.btnSubrayado);
+            this.Controls.Add(this.btnCursiva);
+            this.Controls.Add(this.btnNegrita);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNegrita;
+        private System.Windows.Forms.Button btnCursiva;
+        private System.Windows.Forms.Button btnSubrayado;
+    }
+}
+
---
+++ Practica1Fuentes/Practica1Fuentes/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1Fuentes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNegrita_Click(object sender, EventArgs e)
+        {
+            label1.Font = new Font(label1.Font, FontStyle.Bold);
+        }
+
+        private void btnCursiva_Click(object sender, EventArgs e)
+        {
+            label1.Font = new Font(label1.Font, FontStyle.Italic);
+        }
+
+        private void btnSubrayado_Click(object sender, EventArgs e)
+        {
+            label1.Font = new Font(label1.Font, FontStyle.Underline);
+        }
+    }
+}
