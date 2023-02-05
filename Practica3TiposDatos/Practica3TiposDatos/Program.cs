1e1502bcbc32510cfdd30c7107934f4c84689c7c
yuma-1993 <110123206+yuma-1993@users.noreply.github.com>
2023-02-05 16:33:58 +0100

    Practica 3 - Tipos de datos

---
+++ Practica3TiposDatos/Practica3TiposDatos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica3TiposDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introducir 2 numero por consola
+            Console.WriteLine("Primer número:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo número:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            //Suma
+            int sum = num1 + num2;
+            Console.WriteLine("La suma es: " + sum);
+            //Resta
+            int rest = num1 - num2;
+            Console.WriteLine("La suma es: " + num1 + " - " + num2 + " = " + rest);
+            //Multiplicacion
+            int mult = num1 * num2;
+            Console.WriteLine("La multiplicación es: " + mult);
+            //Division
+            int div = num1 / num2;
+            Console.WriteLine("La división es: " + div);
+            //Residuo
+            int residuo = num1 % num2;
+            Console.WriteLine("El residuo es: " + residuo);
+
+            //Pulsa una tecla para salir
+            Console.WriteLine("Pulse una tecla para salir");
+            Console.ReadLine();
+        }
+    }
+}
