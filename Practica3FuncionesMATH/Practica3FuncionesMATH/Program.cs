fbbe64006b625777235a9cb62e13e0d705cf814a
yuma-1993 <110123206+yuma-1993@users.noreply.github.com>
2023-02-05 16:33:24 +0100

    Practica 3 - Funciones MATH

---
+++ Practica3FuncionesMATH/Practica3FuncionesMATH/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica3FuncionesMATH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            for (int fila = 1; fila <= n; fila++)
+            {
+                for (int col = 1; col <= n - fila; col++)
+                {
+                    Console.Write(" ");
+                }
+                for (int col = 1; col <= 2 * fila - 1; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("adios");
+            Console.Read();
+        }
+    }
+}
