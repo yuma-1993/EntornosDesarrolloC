8ef2bdb91572854a816aeddb2c41015d3527caae
yuma-1993 <110123206+yuma-1993@users.noreply.github.com>
2023-02-05 17:06:58 +0100

    Add files via upload

---
+++ Ejer4.4.3/Ejer4.4.3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejer4._4._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // programa que usa la función Math.PI
+            string name = "Quentin";
+            int x = 3;
+            x = x * 17;
+            double d = Math.PI / 2;
+            MessageBox.Show("name is " + name + "\nx is " + x + "\nd is " + d);
+            // Para qué sirve \n??
+            //SOLUCIÓM: Provoca un salto de linea. En este caso dos.
+            // Qué hace la función PI
+            //SOLUCIÓN: Equivale al número PI que posteriormente es dividido por 2
+            // cuál es el valor de la variable d?
+            //SOLUCIÓN: El valor de la variable d es 1.5707963267948966
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int cont = 0;
+            while (cont < 10)
+            {
+                cont = cont + 1;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                cont = cont - 1;
+            }
+            // cuanto vale cont?
+            //SOLUCIÓN: 5
+            MessageBox.Show("La respuesta es: " + cont);
+        }
+    }
+}
